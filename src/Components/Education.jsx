27087095c28ea06c1b1b3d07ed076af2bc4e3b41
Syed Shahriyar Ali <70702829+SyedShahriyarAli@users.noreply.github.com>
2023-12-01 20:46:47 +0000

    -Added Education Component - Added an extension in container

---
+++ src/Components/Education.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+
+const Education = () => {
+  return (
+    <section className="light" id="education">
+      <h2>Education</h2>
+      <div className="grid">
+        <div className="cell">
+          <h3>University of Engineering and Technology Lahore</h3>
+          <p>BS Computer Science</p>
+          <p>2019 - 2023</p>
+        </div>
+        <div className="cell">
+          <h3>Government College University Lahore</h3>
+          <p>Intermediate</p>
+          <p>2017 - 2019</p>
+        </div>
+        <div className="cell">
+          <h3>Government College University Lahore</h3>
+          <p>Matriculation</p>
+          <p>2015 - 2017</p>
+        </div>
+        <div className="cell">
+          <h3>Government High School</h3>
+          <p>Matriculation</p>
+          <p>2015 - 2017</p>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Education;
