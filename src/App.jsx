3669b763071026f3d4f53dbd7ee82ec4a0aa5057
Syed Shahriyar Ali <70702829+SyedShahriyarAli@users.noreply.github.com>
2023-12-01 20:27:35 +0000

    Updated my personal info and images

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
-  medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  name: "Syed Shahriyar ali",
+  title: "Microsoft Student Ambassador & AI Student",
+  email: "syedshahriyar.ali@studentambassadors.com",
+  gitHub: "SyedShahriyarAli",
+  instagram: "shahriyar_.ali",
+  linkedIn: "syed-shahriyar-ali",
+  medium: "shahriyarali08",
+  twitter: "",
+  youTube: "",
 };
 
 const primaryColor = "#4E567E";
